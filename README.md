49423d991b73746e0eeb34760da3369056dafa48
Steven Nunez <steven.nunez@gmail.com>
2016-02-09 15:11:04 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# ES2015 Playground
+Run this to get started:
+```
+npm install -g babel-cli
+npm install
+```
---
+++ class.js
@@ -0,0 +1,6 @@
+var Person = function(name){
+  this.name = name;
+}
+
+var bob = new Person("Bob");
+console.log(bob.name);
